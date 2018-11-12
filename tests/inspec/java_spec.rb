01e5f526b565e9de7cc09bd8aeca925b414e67ce
diodonfrost <diodon.frost@diodonfrost.me>
2018-11-12 10:57:37 +0100

    add specific inspec test for windows

--- tests/inspec/java_spec.rb
+++ tests/inspec/java_spec.rb
@@ -3,8 +3,14 @@
 control 'java-01' do
   impact 1.0
   title 'Java'
-  desc 'Java should be functuinnal'
-  describe command('java -version') do
-    its('exit_status') { should eq 0 }
+  desc 'Java should be functionnal'
+  if os.family != 'windows'
+    describe command('java -version') do
+      its('exit_status') { should eq 0 }
+    end
+  elsif os.family == 'windows'
+    describe command('java --version') do
+      its('exit_status') { should eq 0 }
+    end
   end
 end
